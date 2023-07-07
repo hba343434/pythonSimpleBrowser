04f9fbe120b855e9696f2bc753971479f6e8541e
hba343434 <120598843+hba343434@users.noreply.github.com>
2023-07-07 22:06:35 +0630

    Create browser.py

---
+++ browser.py
@@ -0,0 +1,29 @@
+##coded by blackkitty
+
+
+import socket
+import argparse
+def browser(target,path,port=80):
+    client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    t=target.split('/')[2]
+    
+    client.connect((t,port))
+    url=f"GET {path} HTTP/1.1\r\nHost: {target}\n\n".encode()
+    
+    client.send(url)
+    while True:
+        data=client.recv(1024)
+        if len(data.decode()) < 1:
+            
+            print('______________end or response___________________')
+            break
+        print('\n'+data.decode())
+
+if __name__=="__main__":
+    parse=argparse.ArgumentParser(description="python simple browser")
+    parse.add_argument('--target','-t',type=str,help='target url')
+    parse.add_argument('--path','-p',type=str,help='url path')
+    var=parse.parse_args()
+   
+    browser(var.target,var.path)
+
